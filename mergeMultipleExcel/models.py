4de4036dc556e1c760ec1d74f8c0bdf1ecf05052
Andronicus Lepcha <andronicus.lepcha@keysight.com>
2024-06-06 10:48:57 +0530

    files are selectable and can be merged now

--- mergeMultipleExcel/models.py
+++ mergeMultipleExcel/models.py
@@ -1,4 +1,5 @@
 from django.db import models
 
 class UploadedFile(models.Model):
+    name=models.CharField(max_length=20)
     file = models.FileField(upload_to='uploads/')
--- mergeMultipleExcel/views.py
+++ mergeMultipleExcel/views.py
@@ -4,20 +4,29 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from openpyxl import Workbook,load_workbook
 from .forms import FileUploadForm
+from .models import UploadedFile
+
 
 def mergeMultipleExcel(request):
+    all_data = UploadedFile.objects.all()
+
     if request.method == "POST":
-        files = request.FILES.getlist("files")
+        files = request.POST.getlist("selected_files")
         output_wb = Workbook()
         output_ws = output_wb.active 
         
-        for uploaded_file in files:
-            wb = load_workbook(uploaded_file)
-            for sheet_name in wb.sheetnames:
-                sheet = wb[sheet_name]
-                
-                for row in sheet.iter_rows(values_only=True):
-                    output_ws.append(row)
+        for uploaded_file_id in files:
+            uploaded_file = UploadedFile.objects.get(pk=uploaded_file_id)
+            file_path = uploaded_file.file.path
+
+            try:
+                wb = load_workbook(file_path)
+                for sheet_name in wb.sheetnames:
+                    sheet = wb[sheet_name]
+                    for row in sheet.iter_rows(values_only=True):
+                        output_ws.append(row)
+            except Exception as e:
+                return HttpResponse(f"Error: {e}")
 
         output_stream = BytesIO()
         output_wb.save(output_stream)
@@ -27,7 +36,7 @@ def mergeMultipleExcel(request):
         response['Content-Disposition'] = 'attachment; filename=consolidated.xlsx'
         return response
 
-    return render(request, "mergeMultipleExcel/upload.html")
+    return render(request, "mergeMultipleExcel/upload.html",{'data':all_data})
 
 def upload_file(request):
     if request.method == 'POST':
@@ -41,3 +50,7 @@ def upload_file(request):
 
 def upload_success(request):
     return render(request, 'upload_success.html')
+
+def show_uploaded_data(request):
+    all_data=UploadedFile.objects.all()
+    return render(request,'mergeMultipleExcel/upload.html',{'data':all_data})
